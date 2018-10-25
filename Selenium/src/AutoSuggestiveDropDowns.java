117d697264356e7d673cf4aa577e455c2d34b661
sriram488 <srajen488@gmail.com>
2018-10-25 18:09:14 -0500

    dropdown dynamic suggestion

--- Selenium/src/AutoSuggestiveDropDowns.java
+++ Selenium/src/AutoSuggestiveDropDowns.java
@@ -1,21 +1,52 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestCase {
+public class AutoSuggestiveDropDowns {
 
 	@Test
-	public void main() {
+	public void autoDropDownTest() throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriram\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\sriram\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com/");
+		driver.get("http://ksrtc.in/oprs-web/");
 		driver.getTitle();
 		System.out.println(driver.getTitle());
 		driver.getCurrentUrl();
 		System.out.println(driver.getCurrentUrl());
-		driver.close();
-		driver.quit();
+		WebElement element = driver.findElement(By.id("fromPlaceName"));
+		element.sendKeys("BENG");
+		element.sendKeys(Keys.ARROW_DOWN);
+		Thread.sleep(1000);
+		// Javascript DOM can extract hidden elements
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String valueinField = "return document.getElementById(\"fromPlaceName\").value;";
+		String textValue = (String) js.executeScript(valueinField);
+		System.out.println(textValue);
+		int i = 0;
+		while (!textValue.equalsIgnoreCase("BENGALURU INTERNATION AIPORT")) {
+			i++;
+			element.sendKeys(Keys.ARROW_DOWN);
+			textValue = (String) js.executeScript(valueinField);
+			System.out.println(textValue);
+			if (i > 10) {
+				break;
+			}
+		}
+		if (i > 10) {
+			System.out.println("text not found in dropdown");
+		} else {
+			System.out.println("text found in dropdown options");
+		}
+
+		// driver.close();
+		// driver.quit();
 	}
 
 }
